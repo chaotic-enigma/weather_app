651f2987501a25b08b5f2a149bfd62af924e8b19
sameer <sameer@localhost.localdomain>
2018-04-20 19:45:21 +0530

    sime trial and error

--- practise.py
+++ practise.py
@@ -3,36 +3,109 @@ import urllib2
 import auth
 
 key = auth.key
+'''
+def get_location(*args):
+	if len(args) == 2:
+		locations = []
+		for i in args:
+			i = i.lower()
+			url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(i)+'&appid='+key
 
-def get_location(name):
-	name = name.lower()
-	url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(name)+'&appid='+key
-	open_url = urllib2.urlopen(url)
-	open_w = json.load(open_url)
-	ll = [i[1] for i in open_w['coord'].items()]
-	lat = ll[0]
-	lon = ll[1]
-	return lat, lon
+			open_url = urllib2.urlopen(url)
+			open_w = json.load(open_url)
+			ll = [i[1] for i in open_w['coord'].items()]
+			locations.append(ll)
+		first = locations[0]
+		second = locations[1]
+		return first, second
+	else:
+		return 'nope'
+print(get_location('paradise', 'hindupur'))
+'''
 
-def get_stuff(name):
-	name = name.lower()
-	url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(name)+'&appid='+key
-	open_url = urllib2.urlopen(url)
-	open_w = json.load(open_url)
+def get_location(name1, name2):
+	lst = [name1, name2]
+	locations = []
+	for i in lst:
+		i = i.lower()
+		url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(i)+'&appid='+key
 
-	clouds = open_w['clouds']
+		open_url = urllib2.urlopen(url)
+		open_w = json.load(open_url)
+		ll = [i[1] for i in open_w['coord'].items()]
+		locations.append(ll)
+	return locations
+#print(get_location('hindupur', 'bangalore')) #[[13.83, 77.49], [12.98, 77.6]]
 
-	cloudy = clouds.values()[0]
-	weather = open_w['weather'][0]['description']
-	temp = open_w['main']['temp']
+loc1 = get_location('hindupur', 'bangalore')[0]
+loc2 = get_location('hindupur', 'bangalore')[1]
 
-	temp_c = temp - 273
-	temp_c = round(temp_c, 2)
+lats = []
+lats.append(loc1[0])
+lats.append(loc2[0])
 
-	temp_f = temp_c * 9/5 + 32
-	temp_f = round(temp_f, 2)
+lons = []
+lons.append(loc1[1])
+lons.append(loc2[1])
 
-	ws = open_w['wind']['speed']
+print lats
+print lons
 
-	return cloudy, weather, temp_c, temp_f, ws
-print get_stuff('bangalore')
+'''
+def get_stuff(*args):
+	if len(args) == 2:
+		description = []
+		for i in args:
+			i = i.lower()
+			url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(i)+'&appid='+key
+			open_url = urllib2.urlopen(url)
+			open_w = json.load(open_url)
+
+			clouds = open_w['clouds']
+
+			cloudy = clouds.values()[0]
+			weather = open_w['weather'][0]['description']
+			temp = open_w['main']['temp']
+
+			temp_c = temp - 273
+			temp_c = round(temp_c, 2)
+
+			temp_f = temp_c * 9/5 + 32
+			temp_f = round(temp_f, 2)
+
+			ws = open_w['wind']['speed']
+
+			description.append([weather, temp_c, ws])
+		return tuple(description)
+	else:
+		return 'nope'
+print(get_stuff('paradise', 'hindupur'))
+'''
+
+def get_stuff(name1, name2):
+	lst = [name1, name2]
+	description = []
+	for i in lst:
+		i = i.lower()
+		url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(i)+'&appid='+key
+		open_url = urllib2.urlopen(url)
+		open_w = json.load(open_url)
+
+		clouds = open_w['clouds']
+
+		cloudy = clouds.values()[0]
+		weather = open_w['weather'][0]['description']
+		temp = open_w['main']['temp']
+
+		temp_c = temp - 273
+		temp_c = round(temp_c, 2)
+
+		temp_f = temp_c * 9/5 + 32
+		temp_f = round(temp_f, 2)
+
+		ws = open_w['wind']['speed']
+
+		description.append([weather, temp_c, ws])
+	return description
+loc1, loc2 = (get_stuff('paradise', 'hindupur'))
+print loc1, loc2
--- trials.py
+++ trials.py
@@ -15,64 +15,95 @@ key = auth.key
 with open('/home/sameer/Desktop/learn_plotly/token.txt','r') as tk:
 	access = tk.read()
 
-def get_location(name):
-	name = name.lower()
-	url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(name)+'&appid='+key
-	open_url = urllib2.urlopen(url)
-	open_w = json.load(open_url)
-	ll = [i[1] for i in open_w['coord'].items()]
-	lat = ll[0]
-	lon = ll[1]
-	return lat, lon
+def get_location(name1, name2):
+	lst = [name1, name2]
+	locations = []
+	for i in lst:
+		i = i.lower()
+		url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(i)+'&appid='+key
 
-def get_stuff(name):
-	name = name.lower()
-	url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(name)+'&appid='+key
-	open_url = urllib2.urlopen(url)
-	open_w = json.load(open_url)
+		open_url = urllib2.urlopen(url)
+		open_w = json.load(open_url)
+		ll = [i[1] for i in open_w['coord'].items()]
+		locations.append(ll)
+	loc1 = locations[0]
+	loc2 = locations[1]
+	return loc1, loc2
 
-	clouds = open_w['clouds']
-	cloudy = clouds.values()[0]
+def get_stuff(name1, name2):
+	lst = [name1, name2]
+	description = []
+	for i in lst:
+		i = i.lower()
+		url = 'http://api.openweathermap.org/data/2.5/weather?q='+str(i)+'&appid='+key
+		open_url = urllib2.urlopen(url)
+		open_w = json.load(open_url)
 
-	weather = open_w['weather'][0]['description']
+		clouds = open_w['clouds']
 
-	temp = open_w['main']['temp']
-	temp_c = temp - 273
-	temp_c = round(temp_c, 2)
+		cloudy = clouds.values()[0]
+		weather = open_w['weather'][0]['description']
+		temp = open_w['main']['temp']
 
-	temp_f = temp_c * 9/5 + 32
-	temp_f = round(temp_f, 2)
+		temp_c = temp - 273
+		temp_c = round(temp_c, 2)
+		#temp_c = str(temp_c) + 'C'
 
-	ws = open_w['wind']['speed']
+		temp_f = temp_c * 9/5 + 32
+		temp_f = round(temp_f, 2)
 
-	return cloudy, weather, temp_c, temp_f, ws
+		ws = open_w['wind']['speed']
+		#ws = str(ws) + 'mph'
+
+		description.append([weather, temp_c, ws])
+	return description
 
 app.layout = html.Div([
-	html.H5('Type city name: '),
-	dcc.Input(id='input', value='', type='text'),
-	#html.Div(id='output-graph'),
+	html.H5('Home city: '),
+	dcc.Input(id='input1', value='', type='text'),
+	#html.Hr(),
+	html.H5('Destination city: '),
+	dcc.Input(id='input2', value='', type='text'),
+	#html.Hr(),
+	html.Div(id='output-graph'),
+	html.Div([
+		html.H4('Global Weather')
+	], style={'textAlign' : 'center'}),
 	dcc.Graph(id='graph')
 ])
 
+#app.config['suppress_callback_exceptions']=True
+
 @app.callback(
 	Output('graph', 'figure'),
-	[Input('input', 'value')]
+	[Input('input1', 'value'), Input('input2', 'value')]
 )
-def update_location(name):
+def update_location(name1, name2):
 	try:
-		total_clouds, weather_type, celsius, farenheit, wind_speed = get_stuff(name)
-		lat, lon = get_location(name)
-		lat = str(lat)
-		lon = str(lon)
+		loc1 = get_location(name1, name2)[0]
+		loc2 = get_location(name1, name2)[1]
+
+		lats = []
+		lats.append(loc1[0])
+		lats.append(loc2[0])
+
+		lons = []
+		lons.append(loc1[1])
+		lons.append(loc2[1])
+
+		home, dest = get_stuff(name1, name2)
+		home.insert(0, 'Home')
+		dest.insert(0, 'Destination')
 		data = go.Data([
 		    go.Scattermapbox(
-		        lat=[lat],
-		        lon=[lon],
-		        mode='markers',
+		        lat=lats,
+		        lon=lons,
+		        mode='markers+lines',
+		        line=dict(width=2),
 		        marker=go.Marker(
-		            size=11
+		            size=11,
 		        ),
-		        text=[str(name).title() + '  ' + str(weather_type) + '  ' + str(celsius) + ' C  ' + str(wind_speed) + ' mph'],
+		        text=[home, dest],
 		        #hoverlabel=dict(bgcolor='rgba(188, 20, 26, 0.5)'),
 		        hoverinfo='text'
 		    )
@@ -80,27 +111,32 @@ def update_location(name):
 
 		layout = go.Layout(
 			#width=600,
-			height=620,
+			height=700,
 		    autosize=True,
 		    showlegend=False,
 		    hovermode='closest',
+		    geo=dict(
+			    projection = dict(type="equirectangular"),
+		    ),
 		    mapbox=dict(
 		        accesstoken=access,
 		        bearing=1,
 		        center=dict(
-		           lat=int(lat.split('.')[0]),
-		           lon=int(lon.split('.')[0])
+		           lat=int(str(lats[1]).split('.')[0]),
+		           lon=int(str(lons[1]).split('.')[0])
 		        ),
 		        pitch=0,
-		        zoom=5
+		        zoom=5,
+		        style='outdoors'
 		    ),
 		)
+
 
 		return {'data' : data, 'layout' : layout}
 
 	except Exception as e:
 		print(str(e))
-'''
+	
 external_css = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 for css in external_css:
 	app.css.append_css({'external_url' : css})
@@ -108,6 +144,7 @@ for css in external_css:
 external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
 for js in external_js:
 	app.scripts.append_script({'external_url' : js})
-'''
+
+
 if __name__ == '__main__':
 	app.run_server(debug=True)
